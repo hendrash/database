e2913cf0c3ed7cecb77ba9ac7664efcba02a6ffb
hendrash <hendrash@mail.gvsu.edu>
2018-11-28 20:47:15 -0500

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------------
+-- Insert data
+-------------------------------------------------------------------------
+insert into Product values('01','hat','3','Y','2','1','hat.com','walmart','where on head');
+insert into Product values('02','hat','1','N','1','5','hat.com','walmart','where on head');
+insert into Product values('03','bat','2','N','2','1','bat.com','dunhams','play baseball');
+insert into Product values('04','axe','3','Y','2','1','axe.com','dunhams','chop somthing');
+insert into Product values('06','axe','4','Y','3','2','axe.com','dunhams','chop somthing');
+insert into Product values('05','mat','1','N','1','4','mat.com','walmart','keep on floor');
+
+insert into Shipping values('01','1','1');
+insert into Shipping values('02','1','2');
+insert into Shipping values('04','2','3');
+insert into Shipping values('05','7','2');
+insert into Shipping values('06','7','3');
+insert into Shipping values('07','1','1');
+insert into Shipping values('08','2','3');
+
+insert into Customers values('tom@mail.com','tom','t  street','00001');
+insert into Customers values('jim@mail.com','jim','j  street','00002');
+insert into Customers values('bob@mail.com','Bob','b  street','00003');
+insert into Customers values('cam@mail.com','cam','c  street','00004');
+insert into Customers values('thm@mail.com','tom','b  street','00005');
+insert into Customers values('max@mail.com','max','c  street','00006');
+
+insert into Seller values('walmart','a street','(616)333-3333','walmart.com');
+insert into Seller values('dunhams','b street','(517)111-1111','dunhams.com');
